6bbb814dc81e33346748c3399674c0e48ea6fead
Ubuntu <ubuntu@ip-172-31-7-100.us-west-2.compute.internal>
2018-08-08 05:18:50 +0000

    Added ingress file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 !#/bin/bash
+CLUSTER=$1
+export CADDRESS='vke cluster $1 | grep Address | awk '{print $2}''
 kubectl create secret generic mysql-pass --from-literal=password=$1
 kubectl create -f wavefront-heapster.yaml
